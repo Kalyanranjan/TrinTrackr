ea59ddfae8f148c12239938b5c96db86712c1802
Kalyan Ranjan Paraju <kalyanranjanp@gmail.com>
2015-11-18 22:32:12 -0500

    Debugging going from WelcomeScreen to Map Screen

--- app/src/main/java/pranav/kalyan/suhas/trintrackr/WelcomeScreenActivity.java
+++ app/src/main/java/pranav/kalyan/suhas/trintrackr/WelcomeScreenActivity.java
@@ -58,17 +58,28 @@ public class WelcomeScreenActivity extends AppCompatActivity {
                 findViewById(R.id.loginStudent).setVisibility(LinearLayout.GONE);
                 findViewById(R.id.loginSelect).setVisibility(LinearLayout.VISIBLE);
             }
-        });;
+        });
 
         // Going inside after login
-        mLoginAsDriverButton = (Button) findViewById(R.id.submitLoginDriverButton);
-        mLoginAsDriverButton.setOnClickListener(new View.OnClickListener() {
+        mSubmitDriverLoginButton = (Button) findViewById(R.id.submitLoginDriverButton);
+        mSubmitDriverLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(WelcomeScreenActivity.this, StudentMapActivity.class);
                 startActivity(i);
             }
         });
+
+        mSubmitStudentLoginButton = (Button) findViewById(R.id.submitLoginStudentButton);
+        mSubmitStudentLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(WelcomeScreenActivity.this, StudentMapActivity.class);
+                startActivity(i);
+            }
+        });
+
+
 
     }
 }
